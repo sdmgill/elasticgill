2947147a10a241c3a379a00b611fda6705ac72b6
Sean Gill <Sean.Gill@portsamerica.com>
2018-09-09 02:10:22 -0700

    Modified elasticgill_app.py

--- elasticgill_app.py
+++ elasticgill_app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, flash, session,redirect,url_for
 
 application=Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/about')
+@application.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('/aws/glue')
+@application.route('/aws/glue')
 def awsglue():
     return render_template('awsglue.html')
 
 
-@app.route('/aws/emr')
+@application.route('/aws/emr')
 def awsemr():
     return render_template('awsemr.html')
 
