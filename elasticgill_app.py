cf0453c55c6220df1626b5dd62082dbe2fa38932
Sean Gill <Sean.Gill@portsamerica.com>
2018-09-09 22:54:12 -0700

    Major changes including bringin in Template from StartBootstrap and converting it to a Flask App.

--- elasticgill_app.py
+++ elasticgill_app.py
@@ -2,25 +2,25 @@ from flask import Flask, render_template, flash, session,redirect,url_for
 
 application=Flask(__name__)
 
-@application.route('/')
+@application.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
 
-@application.route('/about')
+@application.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
 
 
-@application.route('/aws/glue')
-def awsglue():
-    return render_template('awsglue.html')
+@application.route('/post', methods=['GET', 'POST'])
+def post():
+    return render_template('post.html')
 
 
-@application.route('/aws/emr')
-def awsemr():
-    return render_template('awsemr.html')
+#@application.route('/aws/emr')
+#def awsemr():
+    #return render_template('awsemr.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
-    application.run(host='0.0.0.0')
+    #application.run(host='0.0.0.0')
